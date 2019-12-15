950c2b863420bb2d73d02550161033f0c7c73dc6
ibrahimsenan <ibrahim.senan@yahoo.com>
2019-12-15 09:20:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Handle multiple different http requests at a specific url.
 var express = require('express');
+//extract the entire body portion of an incoming request stream and exposes it on the request body
 var bodyParser = require('body-parser');
+//Importing Co2ValuesGeneratorAPi
 const Co2ValuesGeneratorAPi = require("./Backend/APIs/Co2 Values Generator APi/Co2ValuesGeneratorAPi");
+//setting express app
 var app = express();
 
+//initializing backend routes
 app.use(bodyParser.json());
 app.use('/api/v1', Co2ValuesGeneratorAPi);
 
-
+// get request on root level and shows some details in the website as details about the backend
 app.get("/", function (req ,res) {
     res.send(
         '<br/><h1>Welcome to Co2 Gas Generator Backend</h1> \n <br /> ' +
@@ -17,7 +22,7 @@ app.get("/", function (req ,res) {
 
     )
 });
-
+// listening for http requests on port number 4200
 app.listen(process.env.port || 4200, function () {
     console.log("Start Listening To Port 4200");
 });
